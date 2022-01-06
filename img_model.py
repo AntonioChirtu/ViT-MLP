b5598f4431732991129dfbbcb8468c1ab2cab9e5
AntonioChirtu <eu.antonioc@gmail.com>
2022-01-06 12:28:39 +0200

    Changed initial repo

--- img_model.py
+++ img_model.py
@@ -3,6 +3,7 @@ from pathlib import Path
 from typing import Callable, List, Optional
 
 import pytorch_lightning as pl
+import torchmetrics as metrics
 import torch
 from pytorch_lightning.utilities import rank_zero_only
 from torch.utils.data import DataLoader
@@ -10,25 +11,28 @@ from transformers import (AdamW, AutoConfig,
                           AutoModelForSequenceClassification, AutoTokenizer,
                           get_linear_schedule_with_warmup, ViTForImageClassification, ViTFeatureExtractor)
 
+from torch import nn
+import numpy as np
+
 warnings.filterwarnings('ignore')
 
 
 class ImageClassifier(pl.LightningModule):
     def __init__(
-        self,
-        model_name_or_path: str,
-        learning_rate: float = 2e-5,
-        adam_epsilon: float = 1e-8,
-        weight_decay: float = 0.0,
-        warmup_steps: int = 0,
-        predictions_file: str = 'predictions.pt',
-        num_labels: int = 10
+            self,
+            model_name_or_path: str,
+            learning_rate: float = 2e-5,
+            adam_epsilon: float = 1e-8,
+            weight_decay: float = 0.0,
+            warmup_steps: int = 0,
+            predictions_file: str = 'predictions.pt',
+            num_labels: int = 10
     ):
         super().__init__()
         self.save_hyperparameters()
 
         self.model = ViTForImageClassification.from_pretrained(model_name_or_path, num_labels=num_labels)
-        self.accuracy_metric = pl.metrics.Accuracy()
+        self.accuracy_metric = metrics.Accuracy()
 
     def metric(self, preds, labels, mode='val'):
         a = self.accuracy_metric(preds, labels)
@@ -85,3 +89,88 @@ class ImageClassifier(pl.LightningModule):
         self.hparams.save_dir = save_dir
         self.model.save_pretrained(self.hparams.save_dir)
         # self.tokenizer.save_pretrained(self.hparams.save_dir)
+
+
+# class MLP(nn.Module):
+#     '''
+#     Multilayer Perceptron.
+#     '''
+#
+#     def __init__(self, num_labels):
+#         super().__init__()
+#         self.layers = nn.Sequential(
+#             nn.Flatten(),
+#             nn.Linear(224 * 224 * 3, 64),
+#             nn.ReLU(),
+#             nn.Linear(64, 32),
+#             nn.ReLU(),
+#             nn.Linear(32, num_labels)
+#         )
+#
+#     def forward(self, inputs):
+#         '''Forward pass'''
+#         print("HH", inputs)
+#         # pixel_values = pixel_values[0]
+#         return self.layers(inputs[0])
+
+
+class MLP(pl.LightningModule):
+
+    def __init__(self, num_labels):
+        super().__init__()
+        self.layers = nn.Sequential(
+            nn.Flatten(),
+            nn.Linear(224 * 224 * 3, 64),
+            nn.ReLU(),
+            nn.Linear(64, 32),
+            nn.ReLU(),
+            nn.Linear(32, num_labels)
+        )
+        self.ce = nn.CrossEntropyLoss()
+
+        self.test_acc = metrics.Accuracy()
+        self.train_acc = metrics.Accuracy()
+
+    def forward(self, pixel_values):
+        pixel_values = torch.from_numpy(pixel_values[0])
+        # pixel_values = pixel_values.squeeze()
+        return self.layers(pixel_values)
+
+    def training_step(self, batch, batch_idx):
+        feature_extractor = ViTFeatureExtractor.from_pretrained('google/vit-base-patch16-224-in21k')
+        x, y = batch
+        x_features = np.empty(x.shape, dtype=int)
+
+        for idx in range(x.size(0)):
+            temp = feature_extractor(images=x[idx, :, :, :].cpu().data.numpy(), return_tensor="pt")
+            x_features[idx, :, :, :] = temp['pixel_values'][0]
+        x_features = torch.from_numpy(x_features)
+        x_features = x_features.type(torch.FloatTensor)
+        x_features = x_features.view(x_features.size(0), -1)
+        x_features = x_features.to("cuda:0")
+        y_hat = self.layers(x_features)
+        loss = self.ce(y_hat, y)
+        self.log('train_loss', loss)
+        return {'loss' : loss, 'y_pred' : y_hat, 'y_true' : y}
+
+    def training_epoch_end(self, outputs):
+        accuracy = []
+        for out in outputs:
+            accuracy.append(self.train_acc(out['y_pred'], out['y_true']))
+        accuracy = torch.mean(torch.stack(accuracy))
+        print(f"Train Accuracy: {accuracy}")
+
+        # Save the metric
+        self.log('Train_acc_epoch', accuracy, prog_bar=True)
+
+    def test_step(self, batch, batch_idx, dataloader_idx=0):
+        x, y = batch
+        outputs = self.layers(x)
+        loss = self.ce(outputs, y)
+        self.test_acc(torch.argmax(outputs, axis=1), y)
+        self.log("test_loss", loss, prog_bar=True, on_step=True, on_epoch=True)
+        self.log('test_acc', self.test_acc, on_step=True, on_epoch=False)
+
+    def configure_optimizers(self):
+        optimizer = torch.optim.Adam(self.parameters(), lr=1e-4)
+        return optimizer
--- run.py
+++ run.py
@@ -1,12 +1,24 @@
 from transformers import ViTFeatureExtractor, ViTForImageClassification, BatchFeature
-from torch.utils.data import Dataset, DataLoader
+from torch.utils.data import DataLoader, RandomSampler, DistributedSampler, SequentialSampler
 from torchvision.transforms import ToTensor, Normalize, Resize, Compose
 
 import torch
-from torchvision.datasets import CIFAR10
+from torchvision import datasets, transforms
 import pytorch_lightning as pl
+import torchmetrics
+from torchvision.datasets import CIFAR10
 
-from img_model import ImageClassifier
+from img_model import ImageClassifier, MLP
+
+from PIL import Image
+import requests
+import os
+from tqdm import tqdm
+from torch import optim
+from torch import nn
+import numpy as np
+from matplotlib import pyplot as plt
+from pytorch_lightning.callbacks import ModelCheckpoint
 
 
 class SimpleCustomBatch:
@@ -46,34 +58,60 @@ class ViTFeatureExtractorTransforms:
 
 
 if __name__ == '__main__':
-
     model_name_or_path = 'google/vit-base-patch16-224-in21k'
     num_labels = 10
-    batch_size = 24
+    batch_size = 8
     num_workers = 2
     max_epochs = 4
 
-    train_loader = DataLoader( 
-        CIFAR10('./', download=True, transform=ViTFeatureExtractorTransforms(model_name_or_path)),
-        batch_size=batch_size,
-        num_workers=num_workers,
-        pin_memory=True,
-        collate_fn=my_collate
+    transform_train = transforms.Compose([
+        transforms.RandomResizedCrop((224, 224), scale=(0.05, 1.0)),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5]),
+    ])
+    transform_test = transforms.Compose([
+        transforms.Resize((224, 224)),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5]),
+    ])
+
+    # The directory with the test set contains 30% of data (used to train)
+    trainset = datasets.ImageFolder(root="/home/antonio/PycharmProjects/ViT-MLP/Sydney/test",
+                                    transform=transform_train)
+    testset = datasets.ImageFolder(root="/home/antonio/PycharmProjects/ViT-MLP/Sydney/train",
+                                   transform=transform_test)
+
+    train_sampler = RandomSampler(trainset)
+    test_sampler = SequentialSampler(testset)
+    train_loader = DataLoader(trainset,
+                              sampler=train_sampler,
+                              batch_size=batch_size,
+                              num_workers=4,
+                              pin_memory=True)
+    test_loader = DataLoader(testset,
+                             sampler=test_sampler,
+                             batch_size=1,
+                             num_workers=4,
+                             pin_memory=True) if testset is not None else None
+
+    feature_extractor = ViTFeatureExtractor.from_pretrained(model_name_or_path)
+    model = MLP(num_labels=7)
+
+    epochs = 10
+    save_path = r'/home/antonio/PycharmProjects/ViT-MLP/huggingface-vit-finetune/lightning_logs/version_0'
+
+    checkpoint_callback = ModelCheckpoint(
+        dirpath="/home/antonio/PycharmProjects/ViT-MLP/huggingface-vit-finetune/lightning_logs/version_0",
+        filename="best_model",
+        save_top_k=1,
+        mode="min",
     )
-    val_loader = DataLoader( 
-        CIFAR10('./', download=True, train=False, transform=ViTFeatureExtractorTransforms(model_name_or_path)),
-        batch_size=batch_size,
-        num_workers=num_workers,
-        pin_memory=True,
-        collate_fn=my_collate
-    )
-    model = ImageClassifier(model_name_or_path)
-    # HACK - put this somewhere else
-    model.total_steps = (
-        (len(train_loader.dataset) // (batch_size))
-        // 1
-        * float(max_epochs)
-    )
-    pixel_values, labels = next(iter(train_loader))
-    trainer = pl.Trainer(gpus=1, max_epochs=4, precision=16, limit_train_batches=5)
-    trainer.fit(model, train_loader, val_loader)
+
+    pl.seed_everything(42)
+    trainer = pl.Trainer(auto_scale_batch_size='power', gpus=1, deterministic=True, max_epochs=epochs,
+                         auto_lr_find=True, benchmark=True, callbacks=[checkpoint_callback])
+    if not os.path.exists(save_path):
+        trainer.fit(model, train_loader)
+
+    model = model.load_from_checkpoint(save_path + "/best_model-v1.ckpt", num_labels=7)
+    trainer.test(model, test_loader)
